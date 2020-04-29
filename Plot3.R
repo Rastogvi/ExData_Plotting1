5c911b9cfbcb5ea9e586c44e97ff8ad0ae285fd4
Vinita Rastogi <Vinita.Rastogi@graydon-global.com>
2020-04-29 10:18:35 +0100

    Updated

--- Plot3.R
+++ Plot3.R
@@ -18,15 +18,19 @@ glimpse(raw_data)
 
 Final_data <- subset(raw_data, Date >= "2007-02-01" & Date <= "2007-02-02")
 
+glimpse(Final_data)
+
 ## Plot the graph
 
-with(Final_data, plot(Time, Sub_metering_1,type="l", ylab="Energy Sub metering", xlab=""))
-lines(Final_data$Time,Final_data$Sub_metering_2,col = "red")
-lines(Final_data$Time, Final_data$Sub_metering_3,col = "blue")
-legend("topright",
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lwd=c(1,1,1),
-       col = c("black","red", "blue"))
+par(mfrow = c(1,1))
+
+plot(Final_data$Time, Final_data$Sub_metering_1, type = "l",col="black",
+     ylab="Energy Sub metering", xlab="")
+lines(Final_data$Time, Final_data$Sub_metering_2, col="red")
+lines(Final_data$Time, Final_data$Sub_metering_3, col="blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+legend("topright", legend=labels, col=columnlines, lty="solid")
+
 
 ##Copy the plot
 dev.copy(png, file = "plot3.png", width=480, height=480)
